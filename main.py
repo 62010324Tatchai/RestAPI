8bc61bc48e8b9d0db69d55e4735f3fd836fb69c7
Tatchai <62010324@kmitl.ac.th>
2022-06-06 16:07:42 +0700

    Method Get & Post

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI 
+from typing import Optional # module typing class Optional
+from pydantic import BaseModel
 app = FastAPI()
 
+class User(BaseModel):
+    username : str
+    password : str
+    level : Optional[str] = "normal"
 
 @app.get("/")
 async def root():
     return {"message": "Hello Jewy"}
+
+@app.get("/hi")
+def hi(name:str,reply: str):
+    return {"Hi":name, "reply":reply}
+
+@app.get("/items/{item_id}")
+def read(item_id:int,name:str,reply: Optional[str] = None):
+    return {"ID":item_id,"Item":name, "reply":reply}
+
+@app.post("/login")
+def login(user: User):
+    return {"echo":user}
